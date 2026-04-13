6d5aab3a5cf3d0087d4cf6046b674d42d5f9a83c
holytestrail <1134336@gmail.com>
2026-04-13 13:12:10 +0200

    added stats L+App. Click event on L.

--- landingpage/components/sections/try-app-section.tsx
+++ landingpage/components/sections/try-app-section.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import { useState } from "react"
 import { Checkbox } from "@/components/ui/checkbox"
+import { track } from '@vercel/analytics';
 
 export default function TryAppSection() {
   const [consentChecked, setConsentChecked] = useState(false)
@@ -42,6 +43,7 @@ export default function TryAppSection() {
               href="https://beerbank.vercel.app"
               target="_blank"
               rel="noopener noreferrer"
+              onClick={() => track('go_to_app')}
             >
               <Download className="w-5 h-5 mr-2" />
               Download BeerBank
